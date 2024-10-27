6dc76f6097a9ffd3d7d0defd3cfdb21d066a48ef
BeenaBotz <beenkr567@gmail.com>
2024-10-27 09:45:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://hungry-annetta-gokulbotz-2598dfc1.koyeb.app/")
+URL = environ.get("URL", "")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
